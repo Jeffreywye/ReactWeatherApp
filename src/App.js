1df922631ec0846b9b48cf897a642d8f7315cee2
Jeffreywye <jeffreywye7@gmail.com>
2019-12-02 07:08:41 -0500

    basic api fetch done

--- src/App.js
+++ src/App.js
@@ -2,14 +2,59 @@ import React from 'react';
 import Titles from "./components/Titles";
 import Form from "./components/Form";
 import Weather from "./components/Weather";
+require('dotenv').config();
+
+const API_ID = process.env.REACT_APP_API_KEY;
 
 class App extends React.Component {
+
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    humidity: undefined,
+    description: undefined,
+    error: undefined   
+  }
+
+  // this func is called on form submit
+  //arrow fnc allows this keyword to be bound to 
+  //the app class
+  //e parameter stands for event
+  //e.preventDefault signifies a single page app(which react does)
+  getWeather = async (e) => {
+    e.preventDefault();
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const query = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_ID}&units=metric` 
+    // backticks allow us to inject variables for 
+    // template strings
+    const api_call = await fetch(query);
+    const data = await api_call.json();
+    console.log(data);
+    this.setState({
+      temperature: data.main.temp,
+      city: data.name,
+      country: data.sys.country,
+      humidity: data.main.humidity,
+      description: data.weather[0].description
+    });
+  }
+
   render(){
     return (
       <div>
         <Titles/>
-        <Form/>
-        <Weather/>
+        {/* App comp passes getWeather fnc to Form comp */}
+        <Form getWeather={this.getWeather}/>
+        <Weather 
+          temperature={this.state.temperature}
+          city={this.state.city}
+          country={this.state.country}
+          humidity={this.state.humidity}
+          description={this.state.description}
+          error={this.state.error}
+        />
       </div>
     );
   }
